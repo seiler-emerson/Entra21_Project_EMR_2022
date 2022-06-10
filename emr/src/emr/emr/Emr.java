6ce359e8420f7d2962450a4a40809c030a910ebe
Wellitonborges <welliton_borges@hotmail.com>
2022-06-09 22:12:24 -0300

    SUbindo codgo pra Main

--- emr/src/emr/emr/Emr.java
+++ emr/src/emr/emr/Emr.java
@@ -1,16 +1,12 @@
 package emr.emr;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.ArrayList;
 import java.util.Scanner;
 import emr.Main;
-import emr.records.Records;
-import emr.records.Patient;
 
-public class Emr extends Patient {
+public class Emr {
 
 	static Scanner input = new Scanner(System.in);
+
 	public static void emr() {
 
 		byte option;
@@ -31,12 +27,9 @@ public class Emr extends Patient {
 				break;
 			case 1:
 				System.out.println("NOVO ATENDIMENTO SELECIONADO");
-				
-			
 				break;
 			case 2:
 				System.out.println("CONSULTA DE PRONTUARIO SELECIONADA");
-		
 				break;
 			default:
 				System.out.println("Select a valid option!");
@@ -45,9 +38,8 @@ public class Emr extends Patient {
 
 		} while (option != 0);
 
-	
-}
-	
+	}
+
 	public static String displayMenu() {
 		String menu = "||====================================||";
 		menu += "\n||             PRONTUARIO             ||";
@@ -61,121 +53,5 @@ public class Emr extends Patient {
 
 		return menu;
 	}
-	
-	public static void listPatients(ArrayList<Patient> patients) {
-		
-		for(byte count=0;count<patients.size();count++) {
-			System.out.println(count+" - "+patients.get(count).getName());
-		}
-	}
-	
-	private static void consultEMR(ArrayList<Patient> patients) {
-		listPatients(patients);
-		System.out.println("Select the patient number.");
-		byte index = input.nextByte();
-		
-		System.out.println("ELECTRONIC MEDICAL RECORD");
-		System.out.println("Patient Name: "+patients.get(index).getName());
-		System.out.println("CPF: "+patients.get(index).getCpf());
-		System.out.println("Mother Name "+patients.get(index).getNameMother());
-		System.out.println("Father Name "+patients.get(index).getNameFather());
-		System.out.println("Adress "+patients.get(index).getAddress());
-		System.out.println("Genre "+patients.get(index).getGenre());
-		System.out.println("Birth date: "+patients.get(index).getBirth());
-		if(patients.get(index).appointments == null) {			
-			System.out.println("Nenhum atendimento realizado");			
-		} else {	
-			for(byte count=0;count<patients.get(index).appointments.size();count++) {
-				System.out.println("APPOINTMENTS "+(count+1));
-				System.out.println("Service date: "+patients.get(index).getAppointments().get(count).getDate());
-				System.out.println("Service time: "+patients.get(index).getAppointments().get(count).getHour());
-				System.out.println("Anamnesi :"+patients.get(index).getAppointments().get(count).getAnamnesis());
-				System.out.println("Prescription :"+patients.get(index).getAppointments().get(count).getPrescription());
-				System.out.println("Certificate/Declaration :"+patients.get(index).getAppointments().get(count).getCertificate());
-				System.out.println("Forwarding :"+patients.get(index).getAppointments().get(count).getForwarding());
-				System.out.println("Medical release :"+patients.get(index).getAppointments().get(count).getMedicalRelease());
-			} 
-		}		
-	}
-	
-	
-	
-	public static void appointmentRecord(ArrayList<Patient> patients) {
-		listPatients(patients);
-		
-		System.out.println("Select the patient number.");
-		byte index = input.nextByte();
-		System.out.println("You selected the patient"+patients.get(index).getName());
-		
-		LocalDate appointmentDate = LocalDate.now();
-		LocalTime appointmentHour = LocalTime.now();
-		String anamnesis = anamnesisRecord(index);
-		String prescription = prescriptionRecord(index);
-		String certificate = certificateRecord(index);
-		String forwarding = forwardingRecord(index);
-		String medicalRelease = medicalReleaseRecord(index);
-				
-		Appointment appointment = new Appointment(anamnesis, prescription, certificate, forwarding, medicalRelease, appointmentDate, appointmentHour);   
-		patients.get(index).appointments.add(appointment);
-					
-	}
-	
-	public static String anamnesisRecord(byte index) {
-		System.out.println("Describe the patient's history:");
-		String anamnesisDescription = input.next();
-		
-		return anamnesisDescription;
-	}
-	
-	public static String prescriptionRecord(byte index) {
-		System.out.println("Medical prescription required?");
-		System.out.println("1 - YES");
-		System.out.println("2 - NO");
-		byte option = input.nextByte();
-		String prescriptionDescription = null;
-		
-		if(option == 1) {
-			System.out.println("Report Medical prescription:");
-			prescriptionDescription = input.next();
-		};
-		
-		return prescriptionDescription;
-	}
-	
-	public static String certificateRecord(byte index) {
-		System.out.println("Certificate/declaration required?");
-		System.out.println("1 - YES");
-		System.out.println("2 - NO");
-		byte option = input.nextByte();
-		String certificate = null;
-		
-		if(option == 1) {
-			System.out.println("Report the certificate:");
-			certificate = input.next();
-		};
-		
-		return certificate;
-	}
-	
-	public static String forwardingRecord(byte index) {
-		System.out.println("Forwarding require?");
-		System.out.println("1 - YES");
-		System.out.println("2 - NO");
-		byte option = input.nextByte();
-		String forwarding = null;
-		
-		if(option == 1) {
-			System.out.println("Report the forwarding:");
-			forwarding = input.next();
-		};
-		
-		return forwarding;
-	}
-	
-	public static String medicalReleaseRecord(byte index) {
-		System.out.println("Report the medical release: ");
-		String medicalRelease = input.next();
-		
-		return medicalRelease;
-	}
+
 }
