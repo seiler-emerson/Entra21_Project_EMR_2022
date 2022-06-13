ff9ad05d8f08e21ac34ead1946ee413f8675a574
seiler-emerson <seiler.emerson@gmail.com>
2022-06-13 17:25:19 -0300

    Update MenuEMR.java
    Correção na criaçãp do MenuEMR

--- emr/src/br/com/entra21/emr/backend/login/area/emr/MenuEMR.java
+++ emr/src/br/com/entra21/emr/backend/login/area/emr/MenuEMR.java
@@ -1,9 +1,11 @@
 package br.com.entra21.emr.backend.login.area.emr;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 
 import br.com.entra21.emr.backend.Menu;
+import br.com.entra21.emr.backend.login.area.emr.crud.AppointmentCRUD;
+import br.com.entra21.emr.backend.login.area.emr.crud.DoctorCRUD;
+import br.com.entra21.emr.backend.login.area.emr.crud.PatientCRUD;
 
 // RESPONSAVEL POR GERENCIAR OS MENUS DE PACIENTES, MEDICOS E ATENDIMENTOS
 
@@ -13,7 +15,6 @@ public class MenuEMR extends Menu {
 		super(title, options);
 		// TODO Auto-generated constructor stub
 	}
-
 	
 	@Override
 	public byte captureOption() {
@@ -21,13 +22,13 @@ public class MenuEMR extends Menu {
 		byte opcao = super.captureOption();
 		switch (opcao) {
 		case 1:  
-			new MenuEMR("PATIENTS", new ArrayList<String>(Arrays.asList("Create Patient", "Read Patient","Update Patient","Delete Patient", "Details Patient"))).playMenu();
+			new PatientCRUD().playMenu();
 			break;
 		case 2: 
-			new MenuEMR("DOCTORS", new ArrayList<String>(Arrays.asList("Create Doctor", "Read Doctor","Update Doctor","Delete Doctor", "Details Doctor"))).playMenu();
+			new DoctorCRUD().playMenu();
 			break;
 		case 3:
-			new MenuEMR("APPOINTMENS", new ArrayList<String>(Arrays.asList("Create Appointment", "Read Appointment","Update Appointment","Delete Appointment"))).playMenu();
+			new AppointmentCRUD().playMenu();
 			break;
 		default:
 			System.out.println("Select a valid option for menu " + super.getTitle());
@@ -35,5 +36,4 @@ public class MenuEMR extends Menu {
 		}
 		return opcao;
 	}
-
 }
